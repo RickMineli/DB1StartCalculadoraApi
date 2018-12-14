0a711a3068a76bd265807e3ed80b146b186bcb41
Ricardo Mineli <rick_mineli@hotmail.com>
2018-12-14 17:16:50 -0200

    {Aula 20} Adicionado CrossOrigin no Controller

--- calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/controller/CalculadoraController.java
+++ calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/controller/CalculadoraController.java
@@ -3,6 +3,7 @@ package br.com.db1.db1start.aula18.calculadoraapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import br.com.db1.db1start.aula18.calculadoraapi.dto.CalculadoraDTO;
 import br.com.db1.db1start.aula18.calculadoraapi.model.Calculadora;
 import br.com.db1.db1start.aula18.calculadoraapi.service.CalculadoraService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/calculadora")
 public class CalculadoraController {
